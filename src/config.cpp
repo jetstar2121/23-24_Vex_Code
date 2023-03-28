bc7699a626a7761da329377aa003423238e5f043
Patrick Hanley <patrickhanley2006@gmail.com>
2023-03-28 08:29:50 -0500

    Very minor fomatting changes

--- src/config.cpp
+++ src/config.cpp
@@ -5,28 +5,23 @@ using namespace vex;
 brain Brain;
 controller Controller;
 
-//Right
+// Right
 motor front_right(PORT1, ratio18_1);
 motor mid_right(PORT2, ratio18_1);
 motor back_right(PORT3, ratio18_1);
 
-//Left
+// Left
 motor front_left(PORT4, ratio18_1);
 motor mid_left(PORT5, ratio18_1);
 motor back_left(PORT6, ratio18_1);
 
-//Drive Groups
+// Drive Groups
 motor_group right_drive(front_right, mid_right, back_right);
 motor_group left_drive(front_left, mid_left, back_left);
 
 motor_group DT(front_right, mid_right, back_right, front_left, mid_left, back_left);
 
-
 MiniPID drivePID_Left(0.4, 0.0007, 0.6);
 MiniPID drivePID_Right(0.4, 0.0007, 0.6);
 
 MiniPID turnPID(.5, .9, .8);
-
-
-
-
--- src/main.cpp
+++ src/main.cpp
@@ -65,10 +65,7 @@ void usercontrol(void) {
     // Each time through the loop your program should update motor + servo
     // values based on feedback from the joysticks.
 
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    // ........................................................................
+    
 
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
